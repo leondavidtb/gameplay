464c6bbc2195d5d628124a26ec27e262adc60653
Leonardo David <leondavidtb@gmail.com>
2021-06-24 16:48:16 -0300

    Criação das interfaces de detalhes do servidor, interfaces de agendamento, formulários, modal e componentização

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,23 +1,32 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text } from 'react-native';
 
-import { Avatar } from "../Avatar";
-import { styles } from "./styles";
+import { Avatar } from '../Avatar';
+import { styles } from './styles';
 
 export function Profile() {
   return (
     <View style={styles.container}>
-      <Avatar urlImage="https://github.com/leondavidtb.png" />
+
+      <Avatar urlImage="https://github.com/leondavidtb.png"/>
 
       <View>
         <View style={styles.user}>
-          <Text style={styles.greeting}>Olá,</Text>
-
-          <Text style={styles.username}>Leonardo</Text>
+          <Text style={styles.greeting}>
+            Olá,
+          </Text>
+          
+          <Text style={styles.username}>
+            Leonardo
+          </Text>
         </View>
 
-        <Text style={styles.message}>Hoje é dia de vitória</Text>
+        <Text style={styles.message}>
+          Hoje é dia de vitória
+        </Text>
       </View>
+
     </View>
-  );
+  )
+
 }
